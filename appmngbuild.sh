21119d11c27515c8729e099c25877b92c7396fcb
luocheng <cheng.luo@hnair.com>
2017-04-05 16:56:13 +0800

    new docker file

--- appmngbuild.sh
+++ appmngbuild.sh
@@ -3,5 +3,5 @@
 env GOOS=linux go build
 cp appMng pkg
 cd pkg
-docker build -t registry.time-track.cn:8052/luocheng/appmng:0.9 .
-docker push registry.time-track.cn:8052/luocheng/appmng:0.9
+docker build -t registry.time-track.cn:8052/luocheng/appmng:0.1.1 .
+docker push registry.time-track.cn:8052/luocheng/appmng:0.1.1
---
+++ newbaseimg/go.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+tar -C /usr/local -xzf /usr/bin/base/go1.8.linux-amd64.tar.gz
+mkdir $HOME/src
